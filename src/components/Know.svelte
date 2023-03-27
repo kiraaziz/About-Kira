81da82044394b96765ccc880c2ca48df238801f8
kiraaziz <83905214+kiraaziz@users.noreply.github.com>
2023-03-27 01:35:13 +0100

    Update Know.svelte

--- src/components/Know.svelte
+++ src/components/Know.svelte
@@ -11,16 +11,16 @@
 
 <div class="h-max w-full flex items-center justify-start p-4 flex-col" id="Stack">
     <h1 class="text-4xl mt-4 font-bold shadow-xl px-3 py-1 text-white bg-gradient-to-tr from-blue-600 to-red-600 ">My Stack</h1>
-    <div class="w-full h-full my-6 tabs-sm flex items-center justify-start flex-col lg:p-10" id="Stack">
+    <div class="w-full h-full my-6 flex items-center justify-start flex-col lg:p-10" id="Stack">
         <div class="taps lg:m-5">
             {#each types as type , index}
-                <button on:click={()=>{indexType = -5 ; indexType = index  }} class={`tab tab-sm lg:tag-lg tab-bordered text-lg font-medium ${indexType === index && "tab-active"}`}>{type}</button>
+                <button on:click={()=>{indexType = -5 ; indexType = index  }} class={`tab tab-xs lg:tab-lg tab-bordered lg:text-lg font-medium ${indexType === index && "tab-active"}`}>{type}</button>
             {/each}
         </div>
-        <div  class="w-full grid lg:grid-cols-2 lg:grid-cols-1 gap-2 px-5 lg:px-1 py-3">
+        <div  class="w-full grid lg:grid-cols-2 lg:grid-cols-1 lg:gap-2 px-5 lg:px-1 py-3">
             {#each allFramework as val}
                 {#if val.type === indexType}
-                    <div class="bg-gray-800/50 ease-in-out transition duration-100 hover:shadow-xl hover:bg-primary/50 hover:scale-105 hover:-translate-y-1 col-span-1 h-max border-4 rounded-xl border-white p-5 flex items-center justify-center flex-col">
+                    <div class="bg-gray-800/50 ease-in-out transition duration-100 hover:shadow-xl hover:bg-primary/50 hover:scale-105 hover:-translate-y-1 col-span-1 h-max border-4 rounded-xl border-white lg:p-5 p-3 flex items-center justify-center flex-col">
                         <img class="h-40 w-40 mb-4" alt="" src={`/icons/framework/${val.name}.svg`} />
                         <h1 class="text-xl font-bold text-white">{val.name}</h1>
     
