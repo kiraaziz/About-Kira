f2349c7c3808133e678b1265c7166f5b06171f32
kiraaziz <83905214+kiraaziz@users.noreply.github.com>
2023-03-27 01:47:24 +0100

    Update Main.svelte

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -7,9 +7,6 @@
     let x = 100
 </script>
 
-<div class=" z-20 h-full w-full overflow-hidden absolute top-0 right-0  ">
-    <div class="bg-primary h-max transition ease-in-out duration-200 m-auto" style={`scale: ${1- x/100}`}>x</div>
-</div>
 <div on:scroll={scroll} class="overflow-y-auto backdrop-blur-md z-40 lg:grow h-[90vh] lg:h-screen w-screen">
     <slot />
 </div>
